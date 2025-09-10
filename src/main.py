f5d2a228ca4d560020f657c01b5f7586ba82a00d
carson-ops <rochoncarsond@gmail.com>
2025-09-09 22:39:15 -0600

    updated main.py

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ from scipy.io.wavfile import write
 import wavio as wv
 import whisper
 import time
-
+import os
 
 # Sampling frequency
 freq = 44100
 
 # Recording duration
 duration = 5
-filename = "C:/Users/carso/OneDrive/Desktop/voice-to-anything/src/finished_recording.wav"
+filename = "C:/Users/carso/OneDrive/Desktop/voice-to-anything/src/finished_recording.wav" # will make automatic later
 
 # Start recorder with the given values 
 # of duration and sample frequency
@@ -31,6 +31,8 @@ time.sleep(2) # hope this works by waiting for file to be written
 
 model = whisper.load_model("turbo")
 result = model.transcribe(filename)
+os.system('cls' if os.name == 'nt' else 'clear') # universal clear
+
 print(result["text"])
 input("Press Enter to continue...")
 
